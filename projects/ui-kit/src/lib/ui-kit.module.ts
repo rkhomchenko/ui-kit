76b75f77c38f0e99bcbd690bff90699ca173fafc
ShteyBig <r.shteybig@gmail.com>
2023-08-29 15:41:00 +0300

    initial commit inside q9elements

---
+++ projects/ui-kit/src/lib/ui-kit.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { UiKitComponent } from './ui-kit.component';
+
+@NgModule({
+	declarations: [UiKitComponent],
+	imports: [],
+	exports: [UiKitComponent],
+})
+export class UiKitModule {}
